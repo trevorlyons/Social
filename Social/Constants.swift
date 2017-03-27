c816ea6fa0635bbbf6fc5e8d8324dd6456cdea0b
trevorlyons <trevorjclyons@hotmail.com>
2017-03-27 19:39:23 +0200

    Sign in UI custom classes complete

---
+++ Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by Trevor Lyons on 2017-03-27.
+//  Copyright © 2017 Trevor Lyons. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
