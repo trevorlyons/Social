db347576f4d7a20b0ec8a7e51a87558535bca107
trevorlyons <trevorjclyons@hotmail.com>
2017-03-27 16:13:35 +0200

    Completed setting up Firebase integration

---
+++ Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Trevor Lyons on 2017-03-27.
+//  Copyright © 2017 Trevor Lyons. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
